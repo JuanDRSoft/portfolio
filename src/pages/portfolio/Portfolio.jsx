14f2383236b8a92ae08a3913b07ca5dfd87fbf42
JuanDRSoft <95110527+JuanDRSoft@users.noreply.github.com>
2023-01-10 10:59:17 -0500

    feat:  descripcion de portafolio

--- src/pages/portfolio/Portfolio.jsx
+++ src/pages/portfolio/Portfolio.jsx
@@ -3,7 +3,15 @@ import React from "react";
 const Portfolio = () => {
   return (
     <div className="container">
-      <h1>Portafolio</h1>
+      <h1>Portafolio De Desarrollos</h1>
+      <p>
+        Aqui podras encontrar algunos de los proyectos que he realizado, en
+        ellos aplique diferentes tecnologias
+      </p>
+      <p>
+        diferentes tecnologias ( HTML, Node JS, React JS, MongoDB, Tailwind CSS,
+        JavaScript, WordPress, CSS )
+      </p>
     </div>
   );
 };
