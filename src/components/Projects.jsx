aadfcfa7cc91dd1be68cc8c30214320162cbb2c7
JuanDRSoft <95110527+JuanDRSoft@users.noreply.github.com>
2023-01-11 18:18:32 -0500

    feat: animacion a portafolio

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { data } from "../data/portfolioData";
-import PWEASY from "../../src/img/easytraining-web.png";
-import SOLUTIO from "../../src/img/solutio-app.png";
 
 const Projects = () => {
   return (
